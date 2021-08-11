c522d7c07a8f1f9d64bc618fb8fcf0ff87d868fd
GrumpyMonka <40570589+GrumpyMonka@users.noreply.github.com>
2021-08-11 17:12:59 +0300

    created API.cpp

--- Monster/mainwindow.h
+++ Monster/mainwindow.h
@@ -11,24 +11,24 @@ namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow                                   // класс главного окна
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);                              // хз-конструктор
     ~MainWindow();
 
-    void Log(QString str);
-    void showRect(QRect rct, QPen *pen, QBrush *brush);
-    void showPicture(int x, int y, int w, int h, QString name);
-    void ResetScene();
-    void ClearSceneFull();
-    int GraphicsHeight();
-    int GraphicsWidth();
+    void Log(QString str);                                              // функция записи информации в statusBar
+    void showRect(QRect rct, QPen *pen, QBrush *brush);                 // отрисовать квавдрат
+    void showPicture(int x, int y, int w, int h, QString name);         // отрисовать изображение в заданном месте с заданными размерами
+    void ResetScene();                                                  // отрисовать сцену из буфера
+    void ClearSceneFull();                                              // полностью чистит сцену и буфер
+    int GraphicsHeight();                                               // возвращает высоту графического окна
+    int GraphicsWidth();                                                // возвращает ширину графического окна
 
-    void addItem(QVector<QGraphicsItem*> mnstr);
-    void addItem(QVector<QWidget*> mnstr);
+    void addItem(QVector<QGraphicsItem*> mnstr);                        // добавляет графические элементы в буфер
+    void addItem(QVector<QWidget*> mnstr);                              // (перегрузка) добавляет виджеты в буфер
 
 public slots:
 
@@ -36,13 +36,13 @@ public slots:
 private slots:
 
 public:
-    QGraphicsScene *scene;
+    QGraphicsScene *scene;                                              // графическая сцена
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow *ui;                                                 // окно приложения
 
-    QVector<QVector<QGraphicsItem*>> scene_paint_item;
-    QVector<QVector<QWidget*>> scene_paint_widget;
+    QVector<QVector<QGraphicsItem*>> scene_paint_item;                  // буфер графических элементов
+    QVector<QVector<QWidget*>> scene_paint_widget;                      // буфер виджетов
 
 };
 #endif // MAINWINDOW_H
